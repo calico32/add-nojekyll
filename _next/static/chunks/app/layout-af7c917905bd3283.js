8151b4d228fc3517481d0926ac1632367ac87a9b
Romeo Cifuentes-Beach <rcifuentesbeach@ucvts.org>
2024-01-22 01:14:25 -0500

    Static Site

---
+++ _next/static/chunks/app/layout-af7c917905bd3283.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8750:function(e,n,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,3445,23)),Promise.resolve().then(t.t.bind(t,2445,23))},2445:function(){}},function(e){e.O(0,[444,971,938,744],function(){return e(e.s=8750)}),_N_E=e.O()}]);
